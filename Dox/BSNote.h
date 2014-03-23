f2c89e8807a7c9754298c55befe92e4ace52e180
beepscore <support@beepscore.com>
2014-03-23 09:52:16 -0700

    Add BSNote as a subclass of UIDocument.

---
+++ Dox/BSNote.h
@@ -0,0 +1,13 @@
+//
+//  BSNote.h
+//  Dox
+//
+//  Created by Steve Baker on 3/23/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSNote : UIDocument
+
+@end
---
+++ Dox/BSNote.m
@@ -0,0 +1,13 @@
+//
+//  BSNote.m
+//  Dox
+//
+//  Created by Steve Baker on 3/23/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import "BSNote.h"
+
+@implementation BSNote
+
+@end
