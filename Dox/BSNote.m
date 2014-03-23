c7bc9350ff2e17cacdff350c74ee5629ae6aace1
beepscore <support@beepscore.com>
2014-03-23 10:04:34 -0700

    In BSNote override callback methods loadFromContents:ofType:error: and contentsForType:error:

--- Dox/BSNote.m
+++ Dox/BSNote.m
@@ -10,4 +10,34 @@
 
 @implementation BSNote
 
+// Called whenever the application reads data from the file system
+- (BOOL)loadFromContents:(id)contents
+                  ofType:(NSString *)typeName
+                   error:(NSError **)outError
+{
+
+    if ([contents length] > 0) {
+        self.noteContent = [[NSString alloc] initWithBytes:[contents bytes]
+                                                    length:[contents length]
+                                                  encoding:NSUTF8StringEncoding];
+    } else {
+        // When the note is first created, assign some default content
+        self.noteContent = @"Empty"; 
+    }
+    
+    return YES;    
+}
+
+// Called whenever the application (auto)saves the content of a note
+- (id)contentsForType:(NSString *)typeName
+                error:(NSError **)outError
+{
+    if ([self.noteContent length] == 0) {
+        self.noteContent = @"Empty";
+    }
+
+    return [NSData dataWithBytes:[self.noteContent UTF8String]
+                          length:[self.noteContent length]];
+}
+
 @end
