3f0f115f940e5ac8f7b696e409e97fa4671f2191
beepscore <support@beepscore.com>
2014-03-23 11:45:21 -0700

    Add dataReloaded: and observer to call it.

--- Dox/BSViewController.m
+++ Dox/BSViewController.m
@@ -18,12 +18,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(dataReloaded:)
+                                                 name:@"noteModified" object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"noteModified" object:nil];
+}
+
+- (void)dataReloaded:(NSNotification *)notification
+{
+    // Overwrite doc
+    // In general substituting the old content with the new one is NOT a good practice.
+    // When we receive a notification of change from iCloud we should have a conflict resolution policy
+    // to enable the user to accept/refuse/merge the differences
+    // between the local version and the iCloud one.
+    self.doc = notification.object;
+
+    self.noteView.text = self.doc.noteContent;
 }
 
 @end
